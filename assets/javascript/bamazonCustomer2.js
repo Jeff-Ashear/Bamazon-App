ece60fc514d0ceb8eaa735aea2a03201936c721d
Jeff.A <kinesthesiac@gmail.com>
2020-05-05 22:49:36 -0700

    Basically works.  Needs cleanup.

--- assets/javascript/bamazonCustomer2.js
+++ assets/javascript/bamazonCustomer2.js
@@ -17,10 +17,10 @@ let orderTotal = 0;
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    connection.query("SELECT * FROM products", function (err, result, fields) {
+    connection.query("SELECT * FROM products", function (err, result) {
         if (err) throw err;
         console.table(result);
-        console.log("result length: ", result.length)
+        // console.log("result length: ", result.length)
        
         function userInput1() {
             inquirer
@@ -30,10 +30,10 @@ connection.connect(function(err) {
                     message: "Please enter the Item ID of the product you wish to purchase: "
                 }])
                 .then (response => {
-                    console.log("response: ", response)
-                    console.log("response value: ", response.itemID)
+                    // console.log("response: ", response)
+                    // console.log("response value: ", response.itemID)
                     itemID = response.itemID;
-                    console.log("itemID: ", itemID);
+                    // console.log("itemID: ", itemID);
                     if (itemID > result.length) {
                         console.log("Sorry, no such Item ID exists...");
                         userInput1();
@@ -55,10 +55,10 @@ connection.connect(function(err) {
                     message: "Please enter the quantity of this item you would like to purchase: "
                 }])
                 .then (response => {
-                    console.log("response: ", response)
+                    // console.log("response: ", response)
                     desiredQuantity = response.itemQuantity;
-                    console.log("desiredQuantity: ", desiredQuantity)
-                    console.log("item id: ", itemID)
+                    // console.log("desiredQuantity: ", desiredQuantity)
+                    // console.log("item id: ", itemID)
                     connection.query("SELECT stock_quantity FROM products WHERE item_id =" + itemID, function (err, results) {
                         if (err) throw err;
                         console.log("quantity results: ", results)
@@ -69,19 +69,29 @@ connection.connect(function(err) {
                         if (desiredQuantity > availableQuantity) {
                             console.log("Sorry, insuficient quantity of this product to complete this sale.");
                         } else {
-                            console.log("there's plenty of that")
+                            console.log("Here's the details of your order...")
                             connection.query("SELECT price FROM products WHERE item_id =" + itemID, function (err, results) {
                                 if (err) throw err;
-                                console.log("Price results: ", results)
+                                // console.log("Price results: ", results)
                                 itemPrice = results[0].price;
-                                console.log("Item price: ", itemPrice)
+                                // console.log("Item price: ", itemPrice)
                                 orderTotal = itemPrice * desiredQuantity;
-                                console.log("order total: ", orderTotal);
+                                console.log("Order total: ", orderTotal);
+                                console.log("Thank you very much for your buisness.")
+                                availableQuantity = availableQuantity - desiredQuantity;
+                                // console.log("New available quantity: ", availableQuantity)
 
                                 //update database:
-                                connection.query("INSERT INTO stock_quantity FROM products WHERE item_id =" + itemID, function(err, results) {
-                                    
-                                })
+                                connection.query("UPDATE products SET stock_quantity=" + availableQuantity + " WHERE item_id=" + itemID, function (err, results){
+                                    if (err) throw err;
+                                    console.log("Update results: ", results);
+                                });
+                                //double check the database to verify code functionality:
+                                // connection.query("SELECT * FROM products", function(err, results) {
+                                //     if (err) throw err;
+                                //     console.table(results);
+                                // })
+                                connection.end();
                             });
 
                         }
